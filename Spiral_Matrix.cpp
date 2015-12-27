13b9f8e010463c9cfd84ac1f50136aa9b5a3af71
Shupeng Wang <speng.wang@gmail.com>
2015-12-27 20:21:09 +0800

    Update Spiral_Matrix.cpp

--- Spiral_Matrix.cpp
+++ Spiral_Matrix.cpp
@@ -9,24 +9,25 @@ public:
         int left = 0;
         int right = matrix[0].size()-1;
         int x = 1;
-        int total = matrix.size()*matrix[0].size();
+        int total = (matrix.size()*matrix[0].size());
+        //cout << total << endl;
         while(x<=total){
-            for(int i= left;i<=right;i++){
+            for(int i= left;i<=right&&x<=total;i++){
                 ret.push_back(matrix[top][i]);
                 ++x;
             }
             ++top;
-            for(int i=top;i<=bottom;++i){
+            for(int i=top;i<=bottom&&x<=total;++i){
                 ret.push_back(matrix[i][right]);
                 ++x;
             }
             --right;
-            for(int i=right;i>=left;--i){
+            for(int i=right;i>=left&&x<=total;--i){
                 ret.push_back(matrix[bottom][i]);
                 ++x;
             }
             --bottom;
-            for(int i=bottom;i>=top;--i){
+            for(int i=bottom;i>=top&&x<=total;--i){
                 ret.push_back(matrix[i][left]);
                 ++x;
             }
