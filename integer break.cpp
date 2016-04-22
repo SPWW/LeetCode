6ee3735531f5e2277c9827c150951bb23b41bed5
Shupeng Wang <speng.wang@gmail.com>
2016-04-22 10:38:49 +0800

    Create integer break.cpp

---
+++ integer break.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n==2)return 1;
+        if(n==3)return 2;
+        if(n==4)return 4;
+        long ret = 1;
+        while(n>4){
+            ret *=3;
+            n -= 3;
+        }
+        ret *= n;
+        return ret;
+    }
+};
